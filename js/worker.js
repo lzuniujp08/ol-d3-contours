887c4f1f43621ff412a5a2addcd7229013f9276a
Liquid-Zhangliquan <1063754495@qq.com>
2019-11-13 16:12:57 +0800

    fix

--- js/worker.js
+++ js/worker.js
@@ -1,7 +1,5 @@
 onmessage = function (e) {
     console.log('Worker: Message received from main script');
-    // let result = e.data[0] * e.data[1];
-    // const [data,width,height] = e.data;
     const data = e.data.data;
     const width = e.data.width;
     const height = e.data.height;
@@ -12,20 +10,16 @@ onmessage = function (e) {
     //已有点初始二维数组
     var dlen = d.length;
     var matrixData = new Array(width * height);
-
-    /**
-     * 插值矩阵数据,时间复杂度O(height*width*len)
-     */
     var idwcount = 0
     for (var i = 0, k1 = 0; i < height; i++) {
         for (var j = 0; j < width; j++ , k1++) {
             if (!matrixData[k1]) {
                 var sum0 = 0, sum1 = 0;
-                for (var k = 0; k < dlen; k++) {
+                for (var k = dlen - 1; k >= 0; k--) {
                     var dk = d[k];
-                    var dis = ((i - dk.y) * (i - dk.y) + (j - dk.x) * (j - dk.x));
-                    sum0 = sum0 + dk.value * 1 / dis;
-                    sum1 = sum1 + 1 / dis;
+                    var dis = Math.pow((i - dk.y), 2) + Math.pow((j - dk.x), 2);
+                    sum0 += dk.value * 1 / dis;
+                    sum1 += + 1 / dis;
                     idwcount++
                 }
                 if (sum1 != 0)
@@ -40,11 +34,4 @@ onmessage = function (e) {
     var end = new Date().getTime();
     console.log('插值：' + (end - start) / 1000 + '秒');
     postMessage(matrixData);
-    // if (isNaN(result)) {
-    //     postMessage('Please write two numbers');
-    // } else {
-    //     let workerResult = 'Result: ' + result;
-    //     console.log('Worker: Posting message back to main script');
-    //     postMessage(workerResult);
-    // }
 }
